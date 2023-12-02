b859727c87ac9041af61efcd780b85fd9f9f390e
skrlssn <4572326@gmail.com>
2023-12-02 11:24:55 +0100

    day 01: part 2

--- day01/Puzzle.cs
+++ day01/Puzzle.cs
@@ -1,3 +1,9 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestPlatform.CoreUtilities.Extensions;
+
 namespace aoc;
 
 public static class Puzzle
@@ -17,6 +23,46 @@ public static class Puzzle
 
     public static int GetSolutionPart2(IEnumerable<string> input)
     {
-        return 1;
+        string pattern = @"(?:[1-9]|one|two|three|four|five|six|seven|eight|nine)";
+        Regex regexFirst = new Regex(pattern);
+        Regex regexLast = new Regex(pattern, RegexOptions.RightToLeft);
+        Dictionary<string, string> numberDict = new Dictionary<string, string>
+        {
+            {"one", "1"},
+            {"two", "2"},
+            {"three", "3"},
+            {"four", "4"},
+            {"five", "5"},
+            {"six", "6"},
+            {"seven", "7"},
+            {"eight", "8"},
+            {"nine", "9"}
+        };
+
+        var result = new List<int>();
+
+        foreach (var row in input)
+        {
+            MatchCollection matchesFromLeft = regexFirst.Matches(row);
+            MatchCollection matchesFromRight = regexLast.Matches(row);
+
+            string firstDigit = matchesFromLeft.First().ToString();
+            string lastDigit = matchesFromRight.First().ToString();
+
+            if (numberDict.ContainsKey(firstDigit))
+            {
+                firstDigit = numberDict[firstDigit];
+            }
+
+            if (numberDict.ContainsKey(lastDigit))
+            {
+                lastDigit = numberDict[lastDigit];
+            }
+
+            int number = int.Parse(firstDigit + lastDigit);
+
+            result.Add(number);
+        }
+        return result.Sum();
     }
 }
